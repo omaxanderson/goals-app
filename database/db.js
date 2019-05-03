b3f47124e8488d1200397c3e6870576aae68f68d
O Max Anderson <omaxwellanderson@gmail.com>
2019-05-02 22:07:58 -0500

    Add fetchOne db function

--- database/db.js
+++ database/db.js
@@ -2,39 +2,44 @@ import mysql from 'mysql';
 import config from './config';
 
 export default class Db {
-	static query(sql) {
-		return new Promise((resolve, reject) => {
-			const connection = mysql.createConnection(config);
-			connection.connect();
+   static query(sql) {
+      return new Promise((resolve, reject) => {
+         const connection = mysql.createConnection(config);
+         connection.connect();
 
-			connection.query(sql, (err, rows) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(rows);
-				}
-				connection.end();
-			});
-		});
-	}
-	
-	static format(sql, params) {
-		return mysql.format(sql, params);
-	}
+         connection.query(sql, (err, rows) => {
+            if (err) {
+               reject(err);
+            } else {
+               resolve(rows);
+            }
+            connection.end();
+         });
+      });
+   }
 
-	/* Insert a row into the database
-	 *
-	 * @param table String
-	 * @param values Object - contains the key-value pairs that are to be inserted
-	 * @param ignore Boolean - whether or not to INSERT IGNORE
-	 */
-	static insert(table, values, ignore = false) {
-		const columns = Db.format(Object.keys(values).filter(key => values[key]).join(', '));
-		const data = Db.format(Object.values(values).filter(value => value)
-			.map(value => (typeof value !== 'number' ? `'${value}'` : value))
-			.join(', '));
-		const sql = `INSERT ${ignore ? 'IGNORE ' : ''}INTO ${table} (${columns}) VALUES (${data})`;
-		console.log(sql);
-		return this.query(sql);
-	}
+   static format(sql, params) {
+      return mysql.format(sql, params);
+   }
+
+   static async fetchOne(sql) {
+      const result = await this.query(sql);
+      return result[0];
+   }
+
+   /* Insert a row into the database
+    *
+    * @param table String
+    * @param values Object - contains the key-value pairs that are to be inserted
+    * @param ignore Boolean - whether or not to INSERT IGNORE
+    */
+   static insert(table, values, ignore = false) {
+      const columns = Db.format(Object.keys(values).filter(key => values[key]).join(', '));
+      const data = Db.format(Object.values(values).filter(value => value)
+         .map(value => (typeof value !== 'number' ? `'${value}'` : value))
+         .join(', '));
+      const sql = `INSERT ${ignore ? 'IGNORE ' : ''}INTO ${table} (${columns}) VALUES (${data})`;
+      console.log(sql);
+      return this.query(sql);
+   }
 }
