fbf0e9c24a0e34017e61034f2d4fcd1b07e15213
O Max Anderson <omaxwellanderson@gmail.com>
2019-05-06 20:54:43 -0500

    Changed up the custom schedule review component

--- client/src/components/CustomGoalReview.js
+++ client/src/components/CustomGoalReview.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
-import Checkbox from './Checkbox';
+//import Checkbox from './Checkbox';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
 
-class WeeklyGoalReview extends React.Component {
+class CustomGoalReview extends React.Component {
    constructor(props) {
       super(props);
 
       this.state = {
          goal_id: this.props.goal_id,
          completed: this.props.completed || false,
+         value: this.props.goal.amountCompleted || '',
       };
+   }
+
+   componentDidMount() {
+      M.FormSelect.init(document.querySelectorAll('select'), {
+
+      });
    }
 
    handleChange = checked => {
@@ -40,17 +49,41 @@ class WeeklyGoalReview extends React.Component {
       return week.length && week[0].completed;
    }
 
+   getLabel = () => {
+      const {
+         custom_amount,
+         custom_amount_type,
+         custom_per_type
+      } = this.props.goal;
+      const per_type_description = custom_per_type == 'month' ? 'this month'
+         : (custom_per_type == 'week' ? 'this week' : 'today');
+
+      return `How many ${custom_amount_type}s have you \
+         ${['minute', 'hour', 'day'].includes(custom_amount_type) ? 'done' : 'completed the goal'} \
+         ${per_type_description}? (Goal: ${custom_amount})`;
+   }
+
+   test = (e) => {
+      const { value } = e.target;
+      console.log(value, this.props.goal.custom_amount);
+      if (value <= this.props.goal.custom_amount) {
+         this.setState({ value })
+      }
+   }
+
    render() {
-               { /* @TODO this shouldn't be goal_reached, 
-                     but back end isn't sending correct data yet */ }
+      const id = `custom_goal_${this.props.goal.goal_id}_review`;
       return (
-         <div>
-            <p>{this.props.goal.title}</p>
-            <Checkbox
-               onChange={(a) => this.handleChange(a.target.checked)}
-               checked={this.getGoalReachedThisWeek()}
-               label='Did you complete this goal this week?'
-            />
+         <div className='row'>
+            <div className='col s12 m9'>
+               <p>{this.props.goal.title}</p>
+               <div className='input-field'>
+                  <select defaultValue={this.props.goal.completed.amount}>
+                     {[...Array(this.props.goal.custom_amount).keys()].map(num => <option value={num}>{num}</option>)}
+                  </select>
+                  <label htmlFor={id}>{this.getLabel()}</label>
+               </div>
+            </div>
          </div>
       );
    }
@@ -58,4 +91,4 @@ class WeeklyGoalReview extends React.Component {
 
 export default connect(state => ({
 
-}))(WeeklyGoalReview);
+}))(CustomGoalReview);
