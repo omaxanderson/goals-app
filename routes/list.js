ac43067c4d34a1e69e00e543c622ec30729c8fa9
O Max Anderson <omaxwellanderson@gmail.com>
2019-05-29 23:14:47 -0500

    Eslint update

--- routes/list.js
+++ routes/list.js
@@ -4,13 +4,13 @@ import * as listController from '../controllers/List';
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  const result = await listController.index();
-  res.send(JSON.stringify(result));
+   const result = await listController.index();
+   res.send(JSON.stringify(result));
 });
 
 router.put('/', async (req, res, next) => {
-  const result = await listController.update(req.body);
-  res.send(JSON.stringify(result));
+   const result = await listController.update(req.body);
+   res.send(JSON.stringify(result));
 });
 
 export default router;
--- routes/review.js
+++ routes/review.js
@@ -4,12 +4,12 @@ import { setCompleted } from '../controllers/Review';
 const router = express.Router();
 
 router.post('/:goalId', async (req, res, next) => {
-  const result = await setCompleted(req.params.goalId, req.body.completed);
-  console.log(result);
-  res.send(result);
+   const result = await setCompleted(req.params.goalId, req.body.completed);
+   console.log(result);
+   res.send(result);
 });
 
 router.get('/:goalId', async (req, res, next) => {
-  res.send('hello get');
+   res.send('hello get');
 });
 export default router;
